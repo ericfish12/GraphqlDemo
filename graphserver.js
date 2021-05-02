e1639c1477e19fa2916ff2d455f6e1111639636c
ericfish12 <ericfish1221@gmail.com>
2021-05-02 11:16:07 -0400

    mongodb

--- graphserver.js
+++ graphserver.js
@@ -1,78 +1,166 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 const express = require("express");
+const bodyParser = require("body-parser");
 const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
-
+const mongoose = require("mongoose");
+const Event = require("./models/event");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 const schema = buildSchema(`
-type Account{
-name:String
-age:Int
-gender:String
-department:String
-salary(city:String):Int
+ 
+
+type Event {
+_id:ID!
+title:String!
+description:String!
+price:Float!
+date:String!
+creator:User!
 }
-type Query {
-    getClassMates(classNo:Int!):String
-    hello:String
-    accountName:String
-    age:Int
-    account(username:String):Account
+
+type User{
+    _id:ID!
+    email:String!
+    password:String
+    createdEvents:[Event!]
 }
+
+input UserInput{
+    email:String!
+    password:String!
+}
+
+
+input EventInput{
+    title:String!
+    description:String!
+    price:Float!
+    date:String!
+
+    
+}
+
+type RootQuery{
+    events:[Event!]!
+}
+
+type RootMutation {
+createEvent (eventInput:EventInput):Event
+createUser(userInput:UserInput):User
+}
+
+
+
+schema{
+    query:RootQuery
+    mutation:RootMutation
+}
+
+
+
 `);
 
 const root = {
-  getClassMates({classNo}) {
-    const obj = {
-      1: "Alex",
-      2: "Michaelle",
-      3: "Jack",
-      4: "Thanh",
-      5: "Jason",
-      6: "Siyi",
-      7: "Sidney",
-      8: "Laurance",
-    };
+    events: () => {
+        return Event.find()
+            .then((res) => {
+                return res.map((event) => {
+                    return { ...event._doc, _id: event._doc._id.toString() };
+                });
+            })
+            .catch((err) => {
+                throw err;
+            });
+    },
+    createEvent: async (args) => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: "608dcb1a20e18dfb8421d070"
+        });
 
-    return obj[classNo];
-  },
 
-  hello: () => {
-    return "hello world";
-  },
-  accountName: () => {
-    return "One of these days";
-  },
+        let user = await User.findById("608dcb1a20e18dfb8421d070")
+        if (!user) throw new Error("User exists");
+        else {
+            await user.createdEvents.push(event)
+            await user.save()
+            return event
+                .save()
+                .then((res) => {
+                    console.log(res);
 
-  age: () => {
-    return 24;
-  },
-  account({ username }) {
-    const name = username;
-    const gender = "man";
-    const age = 18;
-    const department = "Antra";
-    const salary = ({ city }) => {
-      return 99999;
-    };
+                    return { ...res._doc, _id: res._doc._id.toString() };
+                })
+                .catch((err) => {
+                    console.log(err);
+                    throw err;
+                });
+        }
+    },
+    createUser: async (args) => {
+        let user = await User.findOne({
+            email: args.userInput.email,
+        });
+        if (user) {
+            throw new Error("User exists");
+        } else
+            return bcrypt
+                .hash(args.userInput.password, 12)
+                .then((hashedPassword) => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword,
+                    });
+                    return user.save();
+                })
+                .then((res) => {
+                    return { ...res._doc, password: null, _id: res.id };
+                })
 
-    return {
-      name,
-      age,
-      gender,
-      department,
-      salary,
-    };
-  },
+                .catch((err) => {
+                    throw err;
+                });
+    },
+ 
 };
 
 const app = express();
+// app.use(bodyParser.json())
+app.get("/", (req, res, next) => {
+    res.send("hello!");
+});
+
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
 );
 
 app.use(express.static("public"));
-app.listen(3000);
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pgt19.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+// type Query {
+//     getClassMates(classNo:Int!):String
+//     hello:String
+//     accountName:String
+//     age:Int
+//     account(username:String):Account
+// }
---
+++ models/user.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+    type: String,
+    require: true,
+    },
+    password: {
+    type: String,
+    require: true,
+    },
+
+    createdEvents: [
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Event"
+        }
+    ]
+   
+});
+
+
+module.exports = mongoose.model('User', userSchema);
